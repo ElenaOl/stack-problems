ff437f37640da90a3f2db796dab1a892a4d9a9c0
ElenaOl <pravutiner@gmail.com>
2018-01-21 13:33:44 -0800

    finding missing parenthesis

---
+++ missing.js
@@ -0,0 +1,31 @@
+// 7. Find missing parenthesis in a given expression – 2 * ( 3 + 5(sasdfasdfasd)
+
+
+
+function missing(exp){
+    exp = exp.split("");
+    stack = [];
+    // console.log(exp);
+    // console.log(exp[0]);
+    for(var i=0; i<exp.length; i++){
+      if(exp[i] === '('){
+        stack.push(exp[i])
+      }
+      if(exp[i] === ')'){
+        if(stack.length > 0){
+          stack.pop();
+        }else{
+          return "the parenthesis unmatched";
+        }
+      }
+    }
+    if(stack.length > 0){
+      return "the parenthesis unmatched"
+    }else{
+      return "matched"
+    }
+    
+  }
+  
+  missing("2 * ( 3 + (5((sasdfasdfasd))))");
+  
