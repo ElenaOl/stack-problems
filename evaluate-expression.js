12b2f40f9b2ee2304e61d437d4d0f9ab3f602d97
ElenaOl <pravutiner@gmail.com>
2018-01-22 14:50:14 -0800

    solution for reverse array

---
+++ evaluate-expression.js
@@ -0,0 +1,3 @@
+// 8. Evaluate an expression given only single digits and only 2 operators * and +.
+// If you are given an expression 2*3 + 4*5
+// Then the answer expected is 26
---
+++ reverse-array-stack.js
@@ -0,0 +1,36 @@
+// 9. Reverse a stack and put the reversed value back in the same stack. You can use only one other stack and a temp variable.
+
+//O(n^2) === O(n*2n)
+
+
+function reverseStack(arr1){
+  var arr2=[];
+  for(var k=0; k<arr1.length; k++){
+  var temp = arr1.pop();
+    
+    // all numbers that are not in their place are going to the second array
+    while(arr1.length > k){
+      arr2.push(arr1.pop())
+    }
+    
+    arr1.push(temp);
+    
+    // copy back array 2 into array 1
+    while(arr2.length > 0){
+      arr1.push(arr2.pop())
+    }
+
+  }
+  
+  return arr1;
+}
+
+var arr = [1,2,3,4,5];
+reverseStack(arr);
+
+
+
+// last number in first arr going to temp,
+// all other numbers going to second arr
+// temp being pushed back to first arr
+// second array being pushed back to first array
